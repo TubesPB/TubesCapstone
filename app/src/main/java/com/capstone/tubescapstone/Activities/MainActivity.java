eeb931ef9e0be04b78a88d8ba1ecf6efc9392831
shahnazkhairunnisa <shahnazkhairunnisa2@gmail.com>
2021-09-29 06:05:32 +0700

    .

--- app/src/main/java/com/capstone/tubescapstone/Activities/MainActivity.java
+++ app/src/main/java/com/capstone/tubescapstone/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.capstone.tubescapstone;
+package com.capstone.tubescapstone.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.capstone.tubescapstone.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/capstone/tubescapstone/login.java
+++
@@ -1,5 +0,0 @@
-package com.capstone.tubescapstone;
-
-public class login {
-
-}
